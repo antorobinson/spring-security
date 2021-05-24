11e52ebb3633a9a9e6be260794912166093bc0e2
Sahaya Anto Robinson <anto_robinson@outlook.com>
2021-05-24 17:46:38 +0530

    Spring boot application with default security

---
+++ src/main/java/com/anto/security/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package com.anto.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+	@GetMapping("/")
+	public String getData() {
+		return "Hello";
+	}
+}
